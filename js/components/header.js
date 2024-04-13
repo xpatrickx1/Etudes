75333bd23edc8c903317042c879d321f3fcf92bb
 Patrick <patrick@MacBook-Pro.local>
2024-04-13 14:31:25 +0300

    finish

--- js/components/header.js
+++ js/components/header.js
@@ -18,3 +18,5 @@ $(window).bind('load', function() {
         }
     });
 });
+
+
--- pages/page-front.php
+++ pages/page-front.php
@@ -1,8 +1,8 @@
 
 
 <?php get_header(); ?>
-<section class="top-screen cd-index cd-main-content">
 <?php get_template_part('includes/sections/page-front/preloader')?>
+<section class="top-screen cd-index cd-main-content">
 <?php get_template_part('includes/sections/page-front/top-screen')?>
 </section>
 <?php get_footer(); ?>
